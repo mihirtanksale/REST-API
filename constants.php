d9786037f46cd2d78cbf986671ee4abdeed24ba3
Mihir Tanksale <41685553+mihirtanksale@users.noreply.github.com>
2019-09-13 18:17:25 +0530

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','database_jp');
+
+define('USER_CREATED', 101);
+define('USER_EXISTS', 102);
+define('USER_FAILURE', 103);
+
+define('USER_AUTHENTICATED',201);
+define('USER_NOT_FOUND',202);
